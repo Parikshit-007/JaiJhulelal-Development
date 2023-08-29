6ee922ae280c0726d9595737ab2569fce8bfe80c
Parikshit07 <prkshtpandey12@gmail.com>
2023-08-29 23:16:24 +0530

    page changes

--- main/views/search.py
+++ main/views/search.py
@@ -1,30 +1,32 @@
-from django.shortcuts import render
-from django.db.models import Q
-from main.models import Professional
-from django.shortcuts import render
 from django.db.models import Q
 from main.models import Professional
 from datetime import date
-
+from django.shortcuts import render
 def search(request):
     query = request.GET.get('q', '')
-    name_results = request.GET.get('name_results', '')
-    profession_results = request.GET.get('profession_results', '')
-    address_results = request.GET.get('address_results', '')
+    name_results = request.GET.get('name_results', '').strip()
+    profession_results = request.GET.get('profession_results', '').strip().lower()
+    address_results = request.GET.get('address_results', '').strip()
 
-    professionals = Professional.objects.filter(
-        Q(user__username__icontains=name_results) &
-        (Q(address__icontains=address_results) & Q(profession__iexact=profession_results))
-    )
+    professionals = Professional.objects.all()
+
+    if profession_results:
+        professionals = professionals.filter(profession__iexact=profession_results)
+    if address_results:
+        professionals = professionals.filter(address__icontains=address_results)
+    if name_results:
+        professionals = professionals.filter(user__username__icontains=name_results)
 
     for professional in professionals:
         if not professional.profile_image:
             professional.profile_image = 'path/to/default/image.jpg'
 
-    category_name = request.GET.get('category_name', 'All Categories')  
+    category_name = profession_results if profession_results else 'All Categories'
+    
+    profession_choices = professionals.values_list('profession', flat=True).distinct()
+    
     verified_count = professionals.count()
     result_count = f"{verified_count} Results on {date.today().strftime('%d %B, %Y')}"
-
 
     context = {
         'query': query,
@@ -33,10 +35,8 @@ def search(request):
         'address_results': address_results,
         'professionals': professionals,
         'category_name': category_name,
+        'profession_choices': profession_choices,
         'result_count': result_count,
     }
 
     return render(request, 'search.html', context)
-
-
-
