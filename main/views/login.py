97809fbf205803e16500d34fe35d3e65f46a8e84
Parikshit07 <prkshtpandey12@gmail.com>
2023-08-29 20:47:27 +0530

    page changes

--- main/views/login.py
+++ main/views/login.py
@@ -7,8 +7,12 @@ from django.core.mail import send_mail
 from django.conf import settings
 import random
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from main.models import Professional
 
-# Login View
 # Login View
 def user_login(request):
     if request.method == 'POST':
@@ -17,12 +21,21 @@ def user_login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+            
+            # Check if the user has done the registration
+            try:
+                professional = Professional.objects.get(user=user)
+                if professional.profession is None:
+                    return redirect('request_community_entry')
+            except Professional.DoesNotExist:
+                pass
+            
             return redirect('home')
         else:
             messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
-from django.shortcuts import redirect
-from django.contrib.auth import logout
+
+# ... rest of your code ...
 
 def user_logout(request):
     logout(request)
