3204bf37974c56edbcfa27310666459b70868dde
Parikshit07 <prkshtpandey12@gmail.com>
2023-09-13 20:50:45 +0530

    csrf

--- Jaijhulelal/settings.py
+++ Jaijhulelal/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
